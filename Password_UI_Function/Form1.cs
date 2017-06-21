603e3f7f79f8b1c919ab54800a38ed9cf486942a
Seino <whoroad@126.com>
2017-06-21 17:22:10 +0800

    Add project files.

---
+++ Password_UI_Function/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Password_UI_Function
+{
+    public partial class F_Sign : Form
+    {
+        public F_Sign()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Seino\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Login where Username= '" + textBox1.Text + "' and Password= '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Phone Phone_window = new Phone();
+                //main_window.Show();
+                Phone_window.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please check your Username and Password");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Password_UI_Function/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Password_UI_Function
+{
+    public partial class Phone : Form
+    {
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
